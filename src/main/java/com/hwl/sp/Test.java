2cd25cd02173a8663d11b14ad738190e28580223
hwl <hwl@winasdaq.com>
2018-02-24 12:39:20 +0800

    0.1

--- src/main/java/com/hwl/sp/Test.java
+++ src/main/java/com/hwl/sp/Test.java
@@ -25,15 +25,21 @@ public class Test {
 
         JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
 
-        JavaPairRDD<Integer, Integer> rdd1 = jsc.parallelizePairs(data).cache();
+        JavaPairRDD<Integer, Integer> rdd1 = jsc.parallelizePairs(data);
         JavaPairRDD<Integer, Integer> rdd2 = rdd1.mapToPair(e -> new Tuple2<>(e._2(), e._1()));
 
-        rdd1.join(rdd2).foreachPartition((VoidFunction<Iterator<Tuple2<Integer, Tuple2<Integer, Integer>>>>) tuple2Iterator -> {
-            while (tuple2Iterator.hasNext()) {
-                Tuple2<Integer, Tuple2<Integer, Integer>> line = tuple2Iterator.next();
-                System.out.println(line._1() + "(" + line._2()._1() + "," + line._2()._2() + ")");
-            }
-        });
+//        rdd1.join(rdd2).foreachPartition((VoidFunction<Iterator<Tuple2<Integer, Tuple2<Integer, Integer>>>>) tuple2Iterator -> {
+//            while (tuple2Iterator.hasNext()) {
+//                Tuple2<Integer, Tuple2<Integer, Integer>> line = tuple2Iterator.next();
+//                System.out.println(line._1() + "(" + line._2()._1() + "," + line._2()._2() + ")");
+//            }
+//        });
+
+        List<Tuple2<Integer, Tuple2<Integer, Integer>>> result = rdd1.join(rdd2).collect();
+        for (Tuple2<Integer, Tuple2<Integer, Integer>> line : result) {
+            System.out.println(line._1() + "(" + line._2()._1() + "," + line._2()._2() + ")");
+        }
+
         spark.stop();
     }
 }
