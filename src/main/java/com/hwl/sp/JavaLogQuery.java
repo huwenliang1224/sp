72d402dc189d7808f40a166fd6799600b7d7bfd1
hwl <hwl@winasdaq.com>
2018-02-24 15:27:51 +0800

    0.1

--- src/main/java/com/hwl/sp/JavaLogQuery.java
+++ src/main/java/com/hwl/sp/JavaLogQuery.java
@@ -1,12 +1,11 @@
 package com.hwl.sp;
 
-import scala.Tuple2;
-import scala.Tuple3;
-
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
+import scala.Tuple3;
 
 import java.io.Serializable;
 import java.util.Arrays;
