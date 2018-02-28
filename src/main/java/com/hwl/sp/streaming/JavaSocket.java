7bdf33382c204773d231e4afcc9825a970688af8
hwl <hwl@winasdaq.com>
2018-02-28 14:00:50 +0800

    0.1

--- src/main/java/com/hwl/sp/streaming/JavaSocket.java
+++ src/main/java/com/hwl/sp/streaming/JavaSocket.java
@@ -1,7 +1,9 @@
 package com.hwl.sp.streaming;
 
 import org.apache.spark.*;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.*;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.streaming.*;
 import org.apache.spark.streaming.api.java.*;
 import scala.Tuple2;
@@ -11,11 +13,17 @@ import java.util.Arrays;
 public class JavaSocket {
     public static void main(String[] args) throws InterruptedException {
         // Create a local StreamingContext with two working thread and batch interval of 1 second
-        SparkConf conf = new SparkConf().setMaster("local[1]").setAppName("JavaSocket").setJars(new String[]{"/root/project/sp.jar"});
-        JavaStreamingContext context = new JavaStreamingContext(conf, Durations.seconds(20));
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("JavaTC")
+                .getOrCreate();
+
+        JavaSparkContext javaSparkContext = JavaSparkContext.fromSparkContext(spark.sparkContext());
+
+        JavaStreamingContext javaStreamingContext = new JavaStreamingContext(javaSparkContext, Durations.seconds(10));
 
         // Create a DStream that will connect to hostname:port, like localhost:9999
-        JavaReceiverInputDStream<String> lines = context.socketTextStream("localhost", 9999);
+        JavaReceiverInputDStream<String> lines = javaStreamingContext.socketTextStream("localhost", 9999);
 
         // Split each line into words
         JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator());
@@ -27,7 +35,7 @@ public class JavaSocket {
         // Print the first ten elements of each RDD generated in this DStream to the console
         wordCounts.print();
 
-        context.start();              // Start the computation
-        context.awaitTermination();   // Wait for the computation to terminate
+        javaStreamingContext.start();              // Start the computation
+        javaStreamingContext.awaitTermination();   // Wait for the computation to terminate
     }
 }
